344c8686efe80ffdb028389f2ec7c8a0f0c8955e
Benjamin Klemencic <benjamin.klemencic@fau.de>
2021-09-15 14:51:31 +0200

    2.10.14 (fixes ticket no. 924589)

--- config/config.php
+++ config/config.php
@@ -527,13 +527,13 @@ function formatUnivIS( $txt ){
 		'/\|(.*)\|/m' => '<i>$1</i>',  // |itallic|
 		'/_(.*)_/m' => '<sub>$1</sub>',  // H_2_O
 		'/\^(.*)\^/m' => '<sup>$1</sup>',  // pi^2^
-        '/\[([^\]]*)\]\s{1}(\S*)/mi' => '<a href="$2">$1</a>', // [link text] http...
+        '/\[([^\]]*)\]\s{1}((http|https|ftp|ftps):\/\/\S*)/mi' => '<a href="$2">$1</a>', // [link text] http...
 		'/([^">]?)(mailto:)([^")\s<>]+)/mi' => '$1<a href="mailto:$3">$3</a>', // find mailto:email@address.tld but not <a href="mailto:email@address.tld">mailto:email@address.tld</a>
 		'/\*(.*)\*/m' => '<strong>$1</strong>', // *bold*
 	);
 	
-	$txt = nl2br( $txt );
     $txt = preg_replace( array_keys( $subs ), array_values( $subs ), $txt );
+	$txt = nl2br($txt);
     $txt = make_clickable( $txt );
 	return $txt;
 }
