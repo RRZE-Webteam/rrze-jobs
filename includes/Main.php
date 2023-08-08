755a77606fa8dcf34dd1ed5a891bae5fdaafc3c2
Wolfgang Wiese <xwolf@xwolf.de>
2023-08-08 17:37:28 +0200

    wpautop() bloß niemals wieder anrühren...

--- includes/Main.php
+++ includes/Main.php
@@ -22,7 +22,7 @@ class Main
     {
         $this->pluginFile = $pluginFile;
 
-        remove_filter('the_content', 'wpautop');
+   //     remove_filter('the_content', 'wpautop');
         // add_filter('the_content', 'wpautop', 12);
         // add_filter('the_content', 'wpautop', 99);
         // add_filter('the_content', 'shortcode_unautop', 100);
