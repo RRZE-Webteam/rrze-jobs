63f4a8125f23fb5622867e66a4aac8a2431c0814
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-02-26 12:10:19 +0100

    3.7.1 (FILTER_SANITIZE_STRING ist veraltet)

--- includes/qrcode.php
+++ includes/qrcode.php
@@ -1,4 +1,12 @@
 <?php
 include 'phpqrcode/qrlib.php';
 
-QRcode::png(filter_var($_GET['url'], FILTER_SANITIZE_URL) . '#' . filter_var($_GET['collapse'], FILTER_SANITIZE_STRING));
+if (isset($_GET['url'], $_GET['collapse'])) {
+    $url = filter_var($_GET['url'], FILTER_SANITIZE_URL);
+    $collapse = '#' . htmlspecialchars($_GET['collapse'], ENT_QUOTES, 'UTF-8');
+
+
+    QRcode::png(($url ?? '') . ($collapse ?? ''));
+} else {
+    echo "Error: 'url' and 'collapse' parameters are required.";
+}
