37d9bc4b3452478d885ecf91f6ea09610d87b7fc
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-07-01 17:33:37 +0200

    3.9.6 (added IDs to a href for mailto)

--- rrze-jobs.php
+++ rrze-jobs.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE Jobs
 Plugin URI:      https://gitlab.rrze.fau.de/rrze-webteam/rrze-jobs
 Description:     Embedding job offers from job portal apis via shortcode
-Version:         3.9.5
+Version:         3.9.6
 Author:          RRZE Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v3
