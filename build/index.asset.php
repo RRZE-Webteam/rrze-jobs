8929144608937281fbc824455a1b093dd2be1dc1
Barbara Bothe <barbara.bothe@fau.de>
2026-03-12 17:08:40 +0100

    Anpassung FAU-Elemental

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6478982109e8d5232c37');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '010bcd15017ef55aa9bc');
